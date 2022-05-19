1e3ae183d76c256cdd0716bdcfcd98b00eb635e6
Station Chenqiao <i@cctv3.net>
2022-05-20 00:59:51 +0800

    🐞 修复导入 `AndroidStudio` 的错误 -_-||

--- android/app/src/main/java/com/iPhone14/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/iPhone14/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.iphone14.newarchitecture.components;
+package com.iPhone14.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
@@ -15,22 +15,23 @@ import com.facebook.soloader.SoLoader;
  */
 @DoNotStrip
 public class MainComponentsRegistry {
-  static {
-    SoLoader.loadLibrary("fabricjni");
-  }
+    static {
+        SoLoader.loadLibrary("fabricjni");
+    }
 
-  @DoNotStrip private final HybridData mHybridData;
+    @DoNotStrip
+    private final HybridData mHybridData;
 
-  @DoNotStrip
-  private native HybridData initHybrid(ComponentFactory componentFactory);
+    @DoNotStrip
+    private native HybridData initHybrid(ComponentFactory componentFactory);
 
-  @DoNotStrip
-  private MainComponentsRegistry(ComponentFactory componentFactory) {
-    mHybridData = initHybrid(componentFactory);
-  }
+    @DoNotStrip
+    private MainComponentsRegistry(ComponentFactory componentFactory) {
+        mHybridData = initHybrid(componentFactory);
+    }
 
-  @DoNotStrip
-  public static MainComponentsRegistry register(ComponentFactory componentFactory) {
-    return new MainComponentsRegistry(componentFactory);
-  }
+    @DoNotStrip
+    public static MainComponentsRegistry register(ComponentFactory componentFactory) {
+        return new MainComponentsRegistry(componentFactory);
+    }
 }
--- index.tsx
+++ index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {AppRegistry, StatusBar, View} from 'react-native';
 import createContext from 'zustand/context';
 // @ts-ignore
 import {name as appName} from './app.json';
 import Stacks from './Stacks';
 import {useStore} from './useStore';
+import RNFS from 'react-native-fs';
 
 const StoreContext = createContext();
 
 interface iPhone14Props {}
 
 const iPhone14: React.FC<iPhone14Props> = props => {
+  useEffect(() => {
+    console.log({DocumentPath: RNFS.DocumentDirectoryPath});
+    return function () {};
+  }, [props]);
+  
   return (
     <StoreContext.Provider createStore={() => useStore}>
       <View style={{flex: 1}}>
